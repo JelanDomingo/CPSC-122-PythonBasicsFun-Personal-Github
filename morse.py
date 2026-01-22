ac738c586f67daf134f101278adb393b6277b87c
Jelan Domingo <jrcd2006@gmail.com>
2026-01-22 13:29:49 -0800

    convert english to morse

--- morse.py
+++ morse.py
@@ -51,23 +51,42 @@ def load_file(filename: str) -> list[str]:
     infile.close()
     return lines
 
+def convert_english_to_morse(lines: list[str]) -> str:
+    converted_text = ""
+    for line in lines:
+        line = line.upper()
+        print(repr(line))
+        for char in line:
+            print(repr(char))
+            if char in english_morse_dict:
+                converted_text+= english_morse_dict[char] + " "
+            elif char == " " or char == "\n":
+                converted_text += char
+            else:
+                print("Unrecognized character")
+
+    return converted_text
+
 def main():
     
     
     
-    #print("Enter -m for english to morse and -t for morse to english. \nFollow your command with the input file name and output file name.\CMD>", end="")
-    #user_str = input()
+    print("Enter -m for english to morse and -t for morse to english. \nFollow your command with the input file name and output file name.\CMD>", end="")
+    user_str = input()
     #args = user_str.split(" ")
     #conversion_type, infile_name, outfile_names = args
+    conversion_type = user_str
     
     lines = load_file("english.txt")
     print(lines)
-    #if conversion_type == "-m":
+    if conversion_type == "-m":
         # TO DO: call converted_lines = convert_english_to_morse(lines)
-     #   pass
-    #elif conversion_type == "-t":
+        converted_text = convert_english_to_morse(lines)
+        print(converted_text)
+        pass
+    elif conversion_type == "-t":
         # TO DO: call converted_lines = convert_morse_to_english(lines)
-     #   pass
+        pass
     # TO DO: call save_file(outfile_name, converted_lines)
     
 
